585c093d68ce742fc8fcafe4330fca38e72af753
hieu92264 <104044219+hieu92264@users.noreply.github.com>
2023-03-31 19:37:18 +0700

    Add project files.

---
+++ Fastfood Order/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Fastfood_Order
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BTN_Pho_mai_bo = new System.Windows.Forms.Button();
+            this.BTN_Pho_mai_ga = new System.Windows.Forms.Button();
+            this.BTN_Pho_mai_tom = new System.Windows.Forms.Button();
+            this.BTN_Pho_mai_ca = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(199, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Fastfood Order";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Fastfood_Order.Properties.Resources._101952_200;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(121, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BTN_Pho_mai_ca);
+            this.groupBox1.Controls.Add(this.BTN_Pho_mai_tom);
+            this.groupBox1.Controls.Add(this.BTN_Pho_mai_ga);
+            this.groupBox1.Controls.Add(this.BTN_Pho_mai_bo);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 126);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(458, 137);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh mục món ăn";
+            // 
+            // BTN_Pho_mai_bo
+            // 
+            this.BTN_Pho_mai_bo.ForeColor = System.Drawing.Color.Red;
+            this.BTN_Pho_mai_bo.Location = new System.Drawing.Point(3, 18);
+            this.BTN_Pho_mai_bo.Name = "BTN_Pho_mai_bo";
+            this.BTN_Pho_mai_bo.Size = new System.Drawing.Size(129, 23);
+            this.BTN_Pho_mai_bo.TabIndex = 0;
+            this.BTN_Pho_mai_bo.Text = "Burger Phô mai Bò";
+            this.BTN_Pho_mai_bo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_Pho_mai_bo.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Pho_mai_ga
+            // 
+            this.BTN_Pho_mai_ga.ForeColor = System.Drawing.Color.Red;
+            this.BTN_Pho_mai_ga.Location = new System.Drawing.Point(3, 47);
+            this.BTN_Pho_mai_ga.Name = "BTN_Pho_mai_ga";
+            this.BTN_Pho_mai_ga.Size = new System.Drawing.Size(129, 23);
+            this.BTN_Pho_mai_ga.TabIndex = 1;
+            this.BTN_Pho_mai_ga.Text = "Burger Phô mai Gà";
+            this.BTN_Pho_mai_ga.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_Pho_mai_ga.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Pho_mai_tom
+            // 
+            this.BTN_Pho_mai_tom.ForeColor = System.Drawing.Color.Red;
+            this.BTN_Pho_mai_tom.Location = new System.Drawing.Point(3, 76);
+            this.BTN_Pho_mai_tom.Name = "BTN_Pho_mai_tom";
+            this.BTN_Pho_mai_tom.Size = new System.Drawing.Size(129, 23);
+            this.BTN_Pho_mai_tom.TabIndex = 2;
+            this.BTN_Pho_mai_tom.Text = "Burger Phô mai Tôm";
+            this.BTN_Pho_mai_tom.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_Pho_mai_tom.UseVisualStyleBackColor = true;
+            // 
+            // BTN_Pho_mai_ca
+            // 
+            this.BTN_Pho_mai_ca.ForeColor = System.Drawing.Color.Red;
+            this.BTN_Pho_mai_ca.Location = new System.Drawing.Point(3, 105);
+            this.BTN_Pho_mai_ca.Name = "BTN_Pho_mai_ca";
+            this.BTN_Pho_mai_ca.Size = new System.Drawing.Size(129, 23);
+            this.BTN_Pho_mai_ca.TabIndex = 3;
+            this.BTN_Pho_mai_ca.Text = "Burger Phô mai Cá";
+            this.BTN_Pho_mai_ca.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BTN_Pho_mai_ca.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 343);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "Form1";
+            this.Text = "E-Order Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BTN_Pho_mai_ca;
+        private System.Windows.Forms.Button BTN_Pho_mai_tom;
+        private System.Windows.Forms.Button BTN_Pho_mai_ga;
+        private System.Windows.Forms.Button BTN_Pho_mai_bo;
+    }
+}
+
